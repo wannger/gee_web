0a608d97b40745ea8ef2bb6ee9af08a8ed23ba80
wannger <2363025390@qq.com>
2022-08-23 00:52:02 +0800

    day2

--- main.go
+++ main.go
@@ -1,1 +1,26 @@
-package gee_web
+package main
+
+import (
+	"gee_web/gee"
+	"net/http"
+)
+
+func main() {
+	r := gee.New()
+	r.GET("/", func(c *gee.Context) {
+		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+	})
+
+	r.GET("/hello", func(c *gee.Context) {
+		c.String(http.StatusOK, "hello %s,you are at %s\n", c.Query("name"))
+	})
+
+	r.POST("/login", func(c *gee.Context) {
+		c.JSON(http.StatusOK, gee.H{
+			"username": c.PostForm("username"),
+			"password": c.PostForm("password"),
+		})
+	})
+
+	r.Run(":9999")
+}
